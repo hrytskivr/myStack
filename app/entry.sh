6433cd549df6919b08ac5816fd1c655dd3d80c16
Roman Hrytskiv <roman.hrytskiv.m@gmail.com>
2018-03-26 12:49:07 +0300

    Feature/general improvements (#5)
    Add specific nginx version

--- app/entry.sh
+++ app/entry.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 # check if db is available, if no - container exits with code 1
 # regulated by 'restart_policy' in 'docker-stack.yml'
 until nc -z postgres 5432; do
@@ -7,12 +8,12 @@ until nc -z postgres 5432; do
     exit 1
 done
 
-# change dir
+# change dir into app folder
 cd %APP_NAME%
 
 # make & apply migrations
 python manage.py makemigrations && python manage.py migrate
 
-# add worker user and run application on his behalf
+# add worker user & run application on his behalf
 adduser -D -u 111 worker
 su -c "gunicorn %APP_NAME%.wsgi:application -w 2 -b :8000" worker
