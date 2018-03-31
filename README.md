87989dac90fe10383e8ca1582f85611da43aaa7d
Roman Hrytskiv <roman.hrytskiv.m@gmail.com>
2018-03-31 18:21:56 +0300

    * Add registry service * Add stack network * Add tls script * Add nginx service entry check * Alter nginx Dockerfile * Revamp 'fabfile.py'

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ### About
 This whole thing is set-up with idea of microservices architecture in head. Using docker stack, tls, reverse proxy and 
-    other tools this app will wrap your Django application with a fancy production ribbon.
+    other tools this app will wrap your Django application with a fancy production ribbon :ribbon:
 
 ### Prerequisites
 - Any Django application with structure and settings similar to this one - 
@@ -14,12 +14,14 @@ This whole thing is set-up with idea of microservices architecture in head. Usin
 - **fabric3** (version _1.14_ tested)
 
 ### Usage
+- install software requirements
+- make sure to [**init docker swarm**](https://docs.docker.com/engine/reference/commandline/swarm_init/)
 - clone this repository onto your host machine
 - set required environment variables
 - run `fab init` and watch the magic happen
 
 **NOTE:** there is a set of useful commands defined in [**fabfile.py**](https://git.io/vxElt) 
-    to orchestrate the process, e.g. updating/scaling your app on the fly as easy as - `fab update:app`/`fab scale:app`.
+    to orchestrate the process e.g scaling your app service on the fly as easy as - `fab scale:app,5`.
 
-**NOTE:** default tls certificate will contain no user data and is not CA verified, if you would like to use your own 
+**WARNING:** default tls certificate will contain no user data and is not CA verified, if you would like to use your own 
     certificate please alter [**nginx configuration**](https://git.io/vxEWr 'nginx/sites-enabled/django').
--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,18 @@
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 This script is used to control deployment process and make it as easy as possible for end user.
-Please do not forget to set host machine environment variables listed under 'required info'.
+It is divided into 4 sections:
+    - REQUIRED INFO (environment variables that must be set on host machine before deployment)
+    - MAIN FLOW (this is the main flow of the deployment process)
+    - CONTROL COMMANDS (user can control his stack with this commands after deployment)
+    - CONSTANTS & UTILS (various constants & helper functions - to stick with DRY principles)
 
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 import os
 
 from fabric.api import local
+from fabric.context_managers import lcd
 
-""" REQUIRED INFO """
+### REQUIRED INFO ###
 HOST_IP = os.environ['HOST_IP']  # public ip address of your hosting machine
 REPO_URL = os.environ['REPO_URL']  # git url of your application repository
 REPO_NAME = os.environ['REPO_NAME']  # name of your application repository
@@ -18,83 +23,122 @@ DB_USER = os.environ['DB_USER']
 DB_PASS = os.environ['DB_PASS']
 
 
+### MAIN FLOW ###
 def init():
-    """ use this when deploying the stack for the first time """
+    """ run this when deploying the stack for the first time """
     clone_app()
-    rename()
-    build()
-    storages()
-    up()
-
-
-def scale_app(replicas):
-    """ use this to temporary tweak replicas count of the 'app service'
-    (NOTE: this effect will vanish on next 'stack down' command, use environment variable for permanent set-up) """
-    local(f'docker service scale {APP_NAME}_app={replicas}')
-
-
-def update(type):
-    """ use this to update 'stack/app' code base """
-    if type == "stack":
-        local('git reset --hard && git pull')
-    if type == "app":
-        local(f'cd app/{REPO_NAME} && git reset --hard && git pull')
-    rename()
-    build()
+    insert_variables()
+    generate_tls()
+    create_storages()
+    create_network()
+    deploy_registry()
+    make_images()
     up()
 
 
 def clone_app():
-    local(f'cd app && mkdir {REPO_NAME} && git clone {REPO_URL}')
+    """ this will clone your Django application """
+    with lcd('app'):
+        local(f'mkdir {REPO_NAME} && git clone {REPO_URL}')
 
 
-def rename():
+def insert_variables():
+    """ this will replace template placeholders with provided info """
     local(f'sed -i "s/%COMMIT_HASH%/{get_hash()}/g" docker-stack.yml')
-    local(f'sed -i "s/%APP_NAME%/{APP_NAME}/g" app/Dockerfile app/entry.sh nginx/sites-enabled/django')
-    local(f'sed -i "s/%DB_NAME%/{DB_NAME}/; s/%DB_USER%/{DB_USER}/; s/%DB_PASS%/{DB_PASS}/" postgres/env')
     local(f'sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = [\'{HOST_IP}\']/" app/{REPO_NAME}/{APP_NAME}/settings.py')
+    local(f'sed -i "s/%DB_NAME%/{DB_NAME}/; s/%DB_USER%/{DB_USER}/; s/%DB_PASS%/{DB_PASS}/" postgres/env')
+    local(f'sed -i "s/%REGISTRY_URL%/{REGISTRY_URL}/g" docker-stack.yml')
+    local(f'sed -i "s/%APP_NAME%/{APP_NAME}/g" app/Dockerfile '
+                                              'app/app.sh '
+                                              'registry/registry.sh '
+                                              'nginx/sites-enabled/django '
+                                              'docker-stack.yml')
 
 
-def build():
-    local(f'docker build app/. -t my/app:{get_hash()}')
-    local(f'docker build nginx/. -t my/nginx:{get_hash()}')
+def generate_tls():
+    """ this will run the script which will generate tls keys & certificates for nginx & registry services """
+    with lcd('tls'):
+        local('sh tls.sh')
 
 
-def storages():
-    """ this creates persistent storage directories for psql & nginx services """
-    local('mkdir postgres/storage')
+def create_storages():
+    """ this will create persistent storage directories for postgres, nginx & registry services """
+    local('mkdir postgres/data')
     local('mkdir nginx/logs')
+    local('mkdir registry/store')
 
 
+def create_network():
+    """ this will create encrypted & attachable overlay network """
+    local(f'docker network create --opt encrypted --driver overlay --attachable {APP_NAME}_network')
+
+
+def deploy_registry():
+    """ this will run the script which will run docker registry container """
+    with lcd('registry'):
+        local('sh registry.sh')
+
+
+def make_images():
+    """ this will build & push docker images to the registry """
+    local(f'docker build app/. -t {REGISTRY_URL}/app:{get_hash()} && '
+          f'docker push {REGISTRY_URL}/app:{get_hash()}')
+    local(f'docker build nginx/. -t {REGISTRY_URL}/nginx:{get_hash()} && '
+          f'docker push {REGISTRY_URL}/nginx:{get_hash()}')
+
+
+### CONTROL COMMANDS ###
 def up():
-    """ use this to redeploy the stack after it was removed with 'down'
+    """ use this to bring up the stack after it was removed with 'down' command
     or apply any custom changes made in 'docker-stack.yml' file """
     local(f'docker stack deploy --compose-file docker-stack.yml {APP_NAME}')
 
 
 def down(type):
-    """ use this to remove stack from the host """
+    """ use this to stop & remove the stack """
     if type == "-f":
-        print('Forced!')
+        print('\nForced!')
         local(f'docker stack rm {APP_NAME}')
     else:
-        print('echo WARNING: will delete all the data inside every container, '
-              'except data stored directly on host e.g. "postgres".')
-        choice = input('Proceed? Yes/No\n=> ')
+        print('\nWARNING: will delete all the data inside every container, '
+              'except that are stored as persistent storage e.g. "postgres".')
+        choice = input('\nProceed? Yes/No\n=> ')
         if choice == 'Yes':
-            print('Confirmed!')
+            print('\nConfirmed!')
             local(f'docker stack rm {APP_NAME}')
         elif choice == 'No':
-            print('Aborted!')
+            print('\nAborted!')
         else:
-            print('Wrong input! ')
+            print('\nWrong input! ')
+
+
+def scale(service, replicas):
+    """ use this to temporary tweak replicas count of the given service """
+    print('\nNOTE: this setting will only last until next "down" command!')
+    local(f'docker service scale {APP_NAME}_{service}={replicas}')
+
+
+def update(type):
+    """ use this to update code base either for 'myStack' or your Django application """
+    if type == "stack":
+        local('git reset --hard && git pull')
+    if type == "app":
+        with lcd(f'app/{REPO_NAME}'):
+            local(f'git reset --hard && git pull')
+    insert_variables()
+    make_images()
+    up()
+
+
+def status():
+    """ use this to get stack services status, escape with Ctrl+C """
+    local('watch docker service ls')
+
+
+### CONSTANTS & UTILS ###
+REGISTRY_URL = '127.0.0.1:5000'
 
 
 def get_hash():
     """ return last git commit hash """
     return local('git rev-parse --short HEAD', capture=True)
-
-
-def status():
-    """ get stack services status, escape with Ctrl+C """
-    local('watch docker service ls')
