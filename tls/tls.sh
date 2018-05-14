2ca02118f94a2ea7ebe7c4e8621e67afce4509c1
Roman Hrytskiv <roman.hrytskiv.m@gmail.com>
2018-05-14 12:23:06 +0300

    Hotfix/tls issue (#14)
    * fix ssl
    
    * tlx

--- tls/tls.sh
+++ tls/tls.sh
@@ -5,11 +5,11 @@ mkdir nginx && \
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
             -keyout nginx/nginx.key \
             -out nginx/nginx.crt \
-            -subj "/C=/ST=/L=/O=/CN=/"
+            -subj "/C=blank/ST=blank/L=blank/O=blank/CN=blank/"
 
 # generate tls key & certificate for registry service
 mkdir registry && \
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
             -keyout registry/registry.key \
             -out registry/registry.crt \
-            -subj "/C=/ST=/L=/O=/CN=/"
+            -subj "/C=blank/ST=blank/L=blank/O=blank/CN=blank/"
